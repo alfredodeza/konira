bd57eeda99e0537a4ca3fc6f9978a7f0eb999380
Alfredo Deza <alfredodeza@gmail.com>
2011-02-19 20:06:00 -0500

    cleaning up main and how jargon-script imports and runs stuff

--- jargon/collector.py
+++ jargon/collector.py
@@ -1,13 +1,17 @@
 import os
 import re
 
+
+
 class FileCollector(list):
+
 
     def __init__(self, path, match):
         self.match = match
         self.path = path
         self._collect()
         self.valid_module_name = re.compile(r'[_a-z]\w*\.py$', re.IGNORECASE)
+
 
     def _collect(self):
         for root, dirs, files in os.walk(self.path):
