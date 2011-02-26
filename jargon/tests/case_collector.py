43df3e475ce3d742f8f6362d7f569b3424384cf5
Alfredo Deza <alfredodeza@gmail.com>
2011-02-26 13:32:38 -0500

    adding a bit more tests

--- jargon/tests/case_collector.py
+++ jargon/tests/case_collector.py
@@ -8,3 +8,9 @@ describe "collect paths":
     it "should be a list":
         f = FileCollector(path='/asdf')
         assert isinstance(f, list)
+
+
+    it "does not match normal python files":
+        f = FileCollector(path='/asdf')
+        py_file = "foo.py"
+        assert f.valid_module_name.match(py_file) == None
