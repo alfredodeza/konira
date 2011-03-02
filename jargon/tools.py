ffb01913042ba27501bc38397a7adf128d4fa6cb
Alfredo Deza <alfredodeza@gmail.com>
2011-03-02 13:23:28 -0500

    adds a tools file that holds raises

---
+++ jargon/tools.py
@@ -0,0 +1,38 @@
+
+
+def raises(cls=Exception, message=None):
+    return AssertRaises(cls, message)
+
+
+class AssertRaises(object):
+    def __init__(self, exception_class, message):
+        self._exception_class = exception_class
+        self.message = message
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        success = not exc_type
+        if success:
+            raise AssertionError(
+                'Expected an exception of type %s but got none'
+                % self._exception_class.__name__)
+        else:
+            return self.validate_failure(exc_type, exc_value)
+
+    def validate_failure(self, exc_type, exc_value):
+        wrong_message_was_raised = (self.message and
+                                    self.message != str(exc_value))
+        if wrong_message_was_raised:
+            raise AssertionError(
+                "Expected %s('%s') but got %s('%s')" %
+                 (self._exception_class.__name__,
+                  str(self.message),
+                  exc_type.__name__,
+                  str(exc_value)))
+        elif issubclass(exc_type, self._exception_class):
+            return True
+        else:
+            pass
+
