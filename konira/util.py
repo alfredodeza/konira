39c46f015e758c684c73fcb47fe8555725e9474f
Alfredo Deza <alfredodeza@gmail.com>
2011-03-05 13:20:19 -0500

    removed commented out code

--- konira/util.py
+++ konira/util.py
@@ -1,31 +1,6 @@
 import time
 
 
-#
-#def green(string):
-#    """
-#    Makes incoming string output as green on the terminal
-#    """
-#    color_it = "%s%s%s" % (GREEN, string, ENDS)
-#    return color_it
-#
-#
-#def red(string):
-#    """
-#    Makes incoming string output as red on the terminal
-#    """
-#    color_it = "%s%s%s" % (RED, string, ENDS)
-#    return color_it
-#
-#
-#def bold(string):
-#    """
-#    Makes text bold in the terminal
-#    """
-#    bold_it = "%s%s%s" % (BOLD, string, ENDS)
-#    return bold_it
-
-
 def name_convertion(name):
     name = name.replace('_', ' ').replace('Case', '')
     name = name.capitalize().strip() 
