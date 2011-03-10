349c6f421bf8e452d28713366e46a20c2568c137
Alfredo Deza <alfredodeza@gmail.com>
2011-03-10 10:48:07 -0500

    added a whole bunch of tests for the CLI path parsing

--- konira/tests/case_cli.py
+++ konira/tests/case_cli.py
@@ -1,5 +1,6 @@
 # coding: konira
 
+import os
 import sys
 from cStringIO import StringIO
 from konira    import KoniraCommands
@@ -45,13 +46,67 @@ describe "stdout messages for the command line":
         assert self.mystdout.getvalue() == ""
 
 
-
-describe "getting parsed paths":
+describe "getting tests and paths from arguments":
 
 
     before all:
         self.commands = KoniraCommands(argv=[], parse=False, test=True)
 
 
+    it "returns the path only when it cannot split further":
+        path = "/path/to/my/tests"
+        from_path = self.commands.test_from_path(path)
+        assert from_path.get('path') == path
+        assert len(from_path)        == 1
+
+
+    it "returns a class name if it can split more than once":
+        path = "/path/to/my/tests::myclass"
+        from_path = self.commands.test_from_path(path)
+        assert len(from_path)              == 2
+        assert from_path.get('class_name') == 'Case_myclass'
+        assert from_path.get('path')       == '/path/to/my/tests'
+
+
+    it "returns a valid case class name if it can split the path":
+        path = "/path/to/my/tests::my separated class"
+        from_path = self.commands.test_from_path(path)
+        assert len(from_path)              == 2
+        assert from_path.get('class_name') == 'Case_my_separated_class'
+        assert from_path.get('path')       == '/path/to/my/tests'
+
+
+    it "returns the path class and method name if it can":
+        path = "/path::my class::my method"
+        from_path = self.commands.test_from_path(path)
+        assert len(from_path)               == 3
+        assert from_path.get('class_name')  == 'Case_my_class'
+        assert from_path.get('method_name') == 'it_my_method'
+        assert from_path.get('path')        == '/path'
+
+
+    it "always return the path if it fails to split garbage":
+        path = "/path$$$"
+        from_path = self.commands.test_from_path(path)
+        assert len(from_path)        == 1
+        assert from_path.get('path') == path
+
+
+
+describe "getting parsed paths from arguments":
+
+
+    before all:
+        self.commands = KoniraCommands(argv=[], parse=False, test=True)
+        self.cwd = os.getcwd()
+
+
+    it "returns the current cwd for invalid paths":
+        args = ['/usr/bin/executable', '/a/completely/wrong/path']
+        path_dict = self.commands.path_from_argument(args)
+        assert path_dict.get('path') == self.cwd
+
+
+     
 
 
