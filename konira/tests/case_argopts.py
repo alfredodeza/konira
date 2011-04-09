d1e988ef6a646cdc3e1b29fcf04becdacaebd37b
Alfredo Deza <alfredodeza@gmail.com>
2011-04-08 20:01:30 -0400

    adding tests for the arg parser

---
+++ konira/tests/case_argopts.py
@@ -0,0 +1,53 @@
+# coding: konira
+
+from konira import argopts
+
+
+describe "parsing arguments":
+
+
+    it "removes the first item in the list always":
+        parser = argopts.ArgOpts([])
+        parser.parse_args(['foo'])
+        assert parser.args == []
+
+
+    it "matches an option in arguments":
+        parser = argopts.ArgOpts(['--foo'])
+        parser.parse_args(['/bin/konira', '--foo'])
+        assert parser.args  == ['--foo']
+        assert parser.match == ['--foo']
+
+
+    it "matches arguments with no values":
+        parser = argopts.ArgOpts(['--foo'])
+        parser.parse_args(['/bin/konira', '--foo'])
+        assert parser._arg_count['--foo'] == 0
+        assert parser._count_arg[0]       == '--foo'
+
+
+    it "matches arguments with values":
+        parser = argopts.ArgOpts(['--foo'])
+        parser.parse_args(['/bin/konira', '--foo', 'BAR'])
+        assert parser.match               == ['--foo']
+        assert parser._arg_count['--foo'] == 0
+        assert parser._count_arg[1]       == 'BAR'
+
+
+    it "matches valid configured options only":
+        parser = argopts.ArgOpts(['--fuuu'])
+        parser.parse_args(['/bin/konira', '--foo', '--meh'])
+        assert parser.match == []
+
+    
+    it "matches mixed values and arguments":
+        parser = argopts.ArgOpts(['--foo', '--bar'])
+        parser.parse_args(['/bin/konira', '--foo', 'FOO', '--bar'])
+        assert parser.match == ['--foo', '--bar']
+        assert parser._arg_count['--foo'] == 0
+        assert parser._arg_count['--bar'] == 2
+        assert parser._count_arg[1]       == 'FOO'
+        assert parser._count_arg.get(3)   == None
+
+
+
