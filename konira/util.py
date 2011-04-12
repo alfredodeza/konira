a7894c81ebc3d8a5a2170ed71de4ed8e2bfe4598
Alfredo Deza <alfredodeza@gmail.com>
2011-04-11 21:55:16 -0400

    added an options to Stopwatch: * it returns raw time.time if needed

--- konira/util.py
+++ konira/util.py
@@ -21,21 +21,24 @@ def get_class_name(class_name):
 class StopWatch(object):
  
 
-    def __init__(self):
+    def __init__(self, raw=False):
+        self.raw = raw
         self.start = time.time()
 
 
     def elapsed(self):
-
         _elapsed = str(time.time() - self.start)
-        return _elapsed[:5]
+        if not self.raw:
+            return _elapsed[:5]
+        return _elapsed
 
 
 runner_options = dict(
         first_fail = False,
         capturing  = True,
         traceback  = False,
-        dotted     = False
+        dotted     = False,
+        profiling  = False
         )
 
      
