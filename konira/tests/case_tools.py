36c799bf323e0548b1402f8c32221b84e7303464
Alfredo Deza <alfredodeza@gmail.com>
2011-04-13 20:08:32 -0400

    adds tests for tools.raises

---
+++ konira/tests/case_tools.py
@@ -0,0 +1,28 @@
+# coding: konira
+
+import konira
+from konira.exc import KoniraReassertError
+from konira     import tools
+
+
+describe "raises tool":
+
+    before each:
+        self.r = tools.raises
+
+    it "is True when the exception matches":
+        with self.r(): raise
+
+
+
+describe "assert raises":
+
+    it "raises when the exception does not match":
+        assert_raises = tools.AssertRaises(Exception, None)
+        raises KoniraReassertError: assert_raises.__exit__(None, None, None)
+
+
+    it "is true when the exception matches":
+        assert_raises = tools.AssertRaises(Exception, None)
+        assert assert_raises.__exit__(Exception, None, None)
+
