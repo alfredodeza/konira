bf16f85e9ad8745d96d665376e78eb17a4855a6b
Alfredo Deza <Alfredo.Deza@coxinc.com>
2011-09-02 13:41:52 -0400

    bumping version

--- konira/__init__.py
+++ konira/__init__.py
@@ -11,7 +11,7 @@ from konira.argopts   import ArgOpts
 from konira.output    import ReportResults
 import konira.tools
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 class KoniraCommands(object):
 
