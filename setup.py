8fa1f08526f387bd13085fadbb83372c84f051b8
Alfredo Deza <Alfredo.Deza@coxinc.com>
2011-09-09 19:39:45 -0400

    update setup.py to support py3

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-import konira
 import sys
-from distutils.core import setup
+
+# Python3 needs this
+if sys.version < '3':
+    from distutils.core import setup
+    extra = dict()
+else:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup
+    extra = {'use_2to3':True}
 
 scripts = ['bin/konira']
 
@@ -9,7 +17,7 @@ if sys.platform == 'win32':
 
 setup(
     name             = "konira",
-    version          = konira.__version__,
+    version          = "0.2.0",
     packages         = ['konira', 'konira.ext'],
     scripts          = scripts,
     author           = "Alfredo Deza",
@@ -22,74 +30,18 @@ setup(
                         'Intended Audience :: Developers',
                         'License :: OSI Approved :: MIT License',
                         'Topic :: Software Development :: Build Tools',
+                        'Topic :: Software Development :: Libraries',
+                        'Topic :: Software Development :: Testing',
+                        'Topic :: Utilities',
+                        'Operating System :: MacOS :: MacOS X',
+                        'Operating System :: Microsoft :: Windows',
+                        'Operating System :: POSIX',
                         'Programming Language :: Python :: 2.5',
                         'Programming Language :: Python :: 2.6',
                         'Programming Language :: Python :: 2.7',
+                        'Programming Language :: Python :: 3.0',
+                        'Programming Language :: Python :: 3.1',
+                        'Programming Language :: Python :: 3.2',
                       ],
-    long_description = """
-
-**now with 0 calorie syntactic sugar!**
-
-
-Konira - A Python DSL Testing Framework
-=======================================
-Konira is a tool that allows you to write minimalistic
-descriptions for testing scenarios and unit tests.
-
-It is flexible enough to accommodate Behavioral Driven 
-Development as well as traditional Unit Testing. 
-
-
-
-What does a test case look like?
-------------------------------------
-
-
-::
-
-    describe "a very simple test case for my_module":
-
-        it "has a foo property that is True":
-            assert my_module.foo
-
-
-But this is not valid Python!
----------------------------------
-
-I hear you. It is a DSL. All tests need to specify the ``konira`` encoding
-at the top and they can be executed with the included command line tool.
-
-fast and readable action in the terminal
---------------------------------------------
-
-
-::
-
-    $ konira
-    
-    a very simple test case for my_module
-        It has a foo property that is True
-    
-
-    All specs passed in 0.000 secs.
-
-
-Are you sold yet?
----------------------
-
-Full docs can be found at http://konira.cafepais.com
-If you have any ideas or suggestions ping me @alfredodeza
-
-The name
-------------
-
-*Konira* is one of the names of an ancient Inca God. Konira Wirakocha diguised 
-as a traveler in rags. A trickster, a prankster. No one knew who he was, and the 
-people he passed called him names. Yet as he walked, he created. With a word he 
-made the fields and terraced hillsides. Dropping a reed blossom, he made water flow.
-
-This is a DSL, it looks like Python - it mostly is, but it packs simplicity and makes
-testing look good and easy.
-"""
-
+    **extra
 )
