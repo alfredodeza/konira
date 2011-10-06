9e2c94140f06344c21629a178058d1e00f44a3e5
Alfredo Deza <alfredodeza@gmail.com>
2011-10-05 20:30:02 -0400

    add a tokenizer test

--- konira/tests/case_tokenizer.py
+++ konira/tests/case_tokenizer.py
@@ -357,3 +357,16 @@ describe "translate dsl into valid Python":
         assert result[9][1]   == ''
         assert result[10][1]  == ''
 
+
+    it "translates let into a valid method":
+        line = self.line('let foo = True')
+        result = self.translate(line.readline)
+        assert len(result) == 8
+        assert result[0][1]   == 'def'
+        assert result[1][1]   == '_let_foo'
+        assert result[2][1]   == '('
+        assert result[3][1]   == 'self'
+        assert result[4][1]   == ')'
+        assert result[5][1]   == ':'
+        assert result[6][1]   == 'True'
+        assert result[7][1]   == ''
