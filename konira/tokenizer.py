15abfc275c16ca450af1bebab6841c532d257c60
Alfredo Deza <Alfredo.Deza@coxinc.com>
2011-10-07 15:33:52 -0400

    tear it up into smaller functions that are dispatchable

--- konira/tokenizer.py
+++ konira/tokenizer.py
@@ -27,6 +27,109 @@ def valid_raises(value):
     return False
     
 
+def process_name(result, value, last_token, tokenum, last_type, descr_obj, last_kw):
+    # From Describe to class - includes inheritance
+    if value == 'describe':
+        last_kw = 'describe'
+        result.extend(([tokenum, 'class'],))
+
+    elif last_type == OP and last_kw == 'describe':
+        if descr_obj:
+            result.extend(([NAME, value],
+                           [OP, ')'],))
+            last_kw   = None
+            descr_obj = False
+
+    # Skip if Constructors
+    elif value == 'skip':
+        result.extend(([tokenum, 'def'],))
+
+    elif last_token == 'skip' and value == 'if':
+        result.extend(([tokenum, '_skip_if'],
+                       [OP, '('],
+                       [NAME, 'self'],
+                       [OP, ')']))
+
+    # Before Constructors
+    elif value == 'before':
+        result.extend(([tokenum, 'def'],))
+
+    elif last_token == 'before':
+        result.extend(([tokenum, '_before_%s' % value],
+                       [OP, '('],
+                       [NAME, 'self'],
+                       [OP, ')']))
+
+    # After Constructors
+    elif value == 'after':
+        result.extend(([tokenum, 'def'],))
+
+    elif last_token == 'after':
+        result.extend(([tokenum, '_after_%s' % value],
+                       [OP, '('],
+                       [NAME, 'self'],
+                       [OP, ')']))
+
+    # From it to def
+    elif value == 'it':
+        result.extend(([tokenum, 'def'],))
+    elif last_token == 'it':
+        result.extend(([tokenum, valid_method_name(value)],
+                       [OP, '('],
+                       [NAME, 'self'],
+                       [OP, ')'],))
+
+    # From raises to with konira.tools.raises
+    elif value == 'raises' and valid_raises(last_token):
+        result.extend(([tokenum, 'with konira.tools.raises'],))
+
+    elif last_token == 'raises':
+        result.extend(([OP, '('],
+                       [NAME, value],
+                       [OP, ')'],))
+
+    # From let to attribute
+    elif value == 'let':
+        result.extend(([tokenum, ''],))
+
+    elif last_token == 'let':
+        result.pop()
+        result.extend(([tokenum, '_let_%s' % value],))
+
+    else:
+        result.append([tokenum, value])
+
+    return result, value, last_token, tokenum, last_type, descr_obj, last_kw 
+
+def process_string(result, value, last_token, tokenum, last_type, descr_obj, last_kw):
+    if last_token == 'describe':
+        last_kw   = 'describe'
+        descr_obj = True
+        result.extend(([NAME, valid_class_name(value)],))
+
+    elif last_kw == 'describe':
+        if descr_obj:
+            result.extend(([OP, '('],
+                           [NAME, 'object'],
+                           [OP, ')'],
+                           [OP, ':'],))
+            last_kw   = None
+            descr_obj = False
+
+    elif last_token == 'it':
+        result.extend(([tokenum, valid_method_name(value)],
+                       [OP, '('],
+                       [NAME, 'self'],
+                       [OP, ')'],))
+
+    else:
+        result.append([tokenum, value])
+
+    return result, value, last_token, tokenum, last_type, descr_obj, last_kw 
+
+# dispatcher
+dispatcher={NAME: process_name, STRING: process_string}
+
 def translate(readline):
     result     = []
     last_kw    = None
@@ -34,29 +137,17 @@ def translate(readline):
     last_type  = None
     descr_obj  = False
 
-    for tokenum, value, _, _, _ in generate_tokens(readline):
+    for tokenum, value, _, _, _c in generate_tokens(readline):
+        names = dispatcher.get(tokenum)
 
-        # From Describe to class - includes inheritance
-        if tokenum == NAME and value == 'describe':
-            last_kw = 'describe'
-            result.extend(([tokenum, 'class'],))
-        elif tokenum == STRING and last_token == 'describe':
-            last_kw   = 'describe'
-            descr_obj = True
-            result.extend(([NAME, valid_class_name(value)],))
+        if names:
+            result, fvalue, last_token, ftokenum, last_type, descr_obj, last_kw = names(result, value, last_token, tokenum, last_type, descr_obj, last_kw)
 
         elif tokenum == OP and value == ',' and last_type == STRING and last_kw == 'describe':
             if descr_obj:
                 result.extend(([OP, '('],))
                 last_kw   = 'describe'
                 descr_obj = True
-
-        elif tokenum == NAME and last_type == OP and last_kw == 'describe':
-            if descr_obj:
-                result.extend(([NAME, value],
-                               [OP, ')'],))
-                last_kw   = None
-                descr_obj = False
 
         elif last_type == STRING and last_kw == 'describe':
             if descr_obj:
@@ -67,64 +158,9 @@ def translate(readline):
                 last_kw   = None
                 descr_obj = False
 
-        # Skip if Constructors
-        elif tokenum == NAME and value == 'skip':
-            result.extend(([tokenum, 'def'],))
-
-        elif tokenum == NAME and last_token == 'skip' and value == 'if':
-            result.extend(([tokenum, '_skip_if'],
-                           [OP, '('],
-                           [NAME, 'self'],
-                           [OP, ')']))
-
-        # Before Constructors
-        elif tokenum == NAME and value == 'before':
-            result.extend(([tokenum, 'def'],))
-
-        elif tokenum == NAME and last_token == 'before':
-            result.extend(([tokenum, '_before_%s' % value],
-                           [OP, '('],
-                           [NAME, 'self'],
-                           [OP, ')']))
-
-        # After Constructors
-        elif tokenum == NAME and value == 'after':
-            result.extend(([tokenum, 'def'],))
-
-        elif tokenum == NAME and last_token == 'after':
-            result.extend(([tokenum, '_after_%s' % value],
-                           [OP, '('],
-                           [NAME, 'self'],
-                           [OP, ')']))
-
-        # From it to def
-        elif tokenum == NAME and value == 'it':
-            result.extend(([tokenum, 'def'],))
-        elif tokenum == STRING and last_token == 'it':
-            result.extend(([tokenum, valid_method_name(value)],
-                           [OP, '('],
-                           [NAME, 'self'],
-                           [OP, ')'],))
-
-        # From raises to with konira.tools.raises
-        elif tokenum == NAME and value == 'raises' and valid_raises(last_token):
-            result.extend(([tokenum, 'with konira.tools.raises'],))
-
-        elif tokenum == NAME and last_token == 'raises':
-            result.extend(([OP, '('],
-                           [NAME, value],
-                           [OP, ')'],))
-
-        # From let to attribute
-        elif tokenum == NAME and value == 'let':
-            result.extend(([tokenum, ''],))
-
-        elif tokenum == NAME and last_token == 'let':
-            result.pop()
-            result.extend(([tokenum, '_let_%s' % value],))
-
         else:
             result.append([tokenum, value])
+
         last_token = value
         last_type  = tokenum
 
